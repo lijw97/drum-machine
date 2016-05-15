1c8b3d888261a50163001df5eb3fd61a1ff60a95
Jeffrey <lijw@berkeley.edu>
2016-05-15 02:17:13 -0400

    testing soundboard

--- src/Sound.java
+++ src/Sound.java
@@ -9,15 +9,44 @@ import javazoom.jl.player.*;
  * Created by Jeffrey Li on 5/15/2016.
  */
 
-public class Sound {
-    File soundpath;
-    FileInputStream fis_sound;
-    BufferedInputStream bis_sound;
-    String soundname;
-    boolean play = false;
+public class Sound implements Runnable{
+    private File soundpath;
+    private FileInputStream fis_sound;
+    private BufferedInputStream bis_sound;
+    private String soundname;
+    private boolean[] arrayplay;
+
+    public File getSoundpath() {
+        return soundpath;
+    }
+
+    public void setSoundpath(File soundpath) {
+        this.soundpath = soundpath;
+    }
+
+    public String getSoundname() {
+        return soundname;
+    }
+
+    public void setSoundname(String soundname) {
+        this.soundname = soundname;
+    }
+
+    public boolean[] getArrayplay() {
+        return arrayplay;
+    }
+
+    public void setArrayplay(boolean[] arrayplay) {
+        this.arrayplay = arrayplay;
+    }
+
     public Sound(String soundpath, String soundname) {
         this.soundpath = new File(soundpath);
         this.soundname = soundname;
+        arrayplay = new boolean[16];
+        for (int i = 0; i < 16; i++) {
+            arrayplay[i] = false;
+        }
         try {
             fis_sound = new FileInputStream(this.soundpath);
             bis_sound = new BufferedInputStream(fis_sound);
@@ -26,14 +55,26 @@ public class Sound {
         }
     }
     public void run() {
-        if (!play) {
-            try {
-                Player play = new Player(bis_sound);
-                play.play();
+        try {
+            Player play = new Player(bis_sound);
+            play.play();
 
-            } catch (JavaLayerException jexcep) {
-                System.out.println("Problem in run in Sound class");
-            }
+        } catch (JavaLayerException jexcep) {
+            System.out.println("Problem in run in Sound class");
         }
     }
+    public void run(int index) {
+        if (arrayplay[index]) {
+            run();
+        }
+    }
+
+    public void printArray() {
+        System.out.print("[");
+        for (Boolean bool : arrayplay) {
+            System.out.print(" " + bool);
+        }
+        System.out.println("]");
+    }
+
 }
---
+++ src/SoundBoard.java
@@ -0,0 +1,62 @@
+/**
+ * Created by Jeffrey Li on 5/15/2016.
+ */
+
+import java.util.concurrent.CyclicBarrier;
+
+import java.util.ArrayList;
+public class SoundBoard {
+    public ArrayList<Sound> soundboard = new ArrayList();
+    public SoundBoard() {
+        soundboard.add(new Sound("C:\\Users\\Jeffrey Li\\drum-machine\\src\\clap.mp3", "Clap"));
+        soundboard.add(new Sound("C:\\Users\\Jeffrey Li\\drum-machine\\src\\kick.mp3", "Kick"));
+        soundboard.add(new Sound("C:\\Users\\Jeffrey Li\\drum-machine\\src\\open-hihat.mp3", "Open-Hihat"));
+        soundboard.add(new Sound("C:\\Users\\Jeffrey Li\\drum-machine\\src\\snare1.mp3", "Snare"));
+        soundboard.add(new Sound("C:\\Users\\Jeffrey Li\\drum-machine\\src\\closed-hihat.mp3", "Closed-Hihat"));
+    }
+    public void changePlayStatus(int index, String soundName) {
+        Sound selectedSound = null;
+
+        for (int i = 0; i < soundboard.size(); i++) {
+            if (soundName == soundboard.get(i).getSoundname()) {
+                selectedSound = soundboard.get(i);
+            }
+        }
+        try {
+            selectedSound.getArrayplay()[index] = !(selectedSound.getArrayplay()[index]);
+        } catch (NullPointerException exception) {
+            System.out.println("soundboard did not contain sound!");
+        }
+    }
+   public void run() {
+       for (int i = 0; i < 16; i++) {
+           int amounttrue = 0;
+           for (int j = 0; j < soundboard.size(); j++) {
+               if (soundboard.get(j).getArrayplay()[i] == true) {
+                   amounttrue += 1;
+               }
+           }
+           //CyclicBarrier gate = new CyclicBarrier(amounttrue);
+           if (amounttrue != 0) {
+               soundboard.get(0).run(0);
+               soundboard.get(3).run(0);
+           }
+//           try {
+//               Thread.sleep(400);
+//           } catch (InterruptedException exception) {
+//               System.out.println("Program stopped.");
+//           }
+       }
+   }
+
+    public static void main(String[] args) {
+        SoundBoard board = new SoundBoard();
+        board.changePlayStatus(0, "Clap");
+        board.changePlayStatus(0, "Snare");
+        for (Sound s : board.soundboard) {
+            s.printArray();
+        }
+        board.run();
+    }
+
+}
