f873bdb5f2d2dc172951f69e367c5bcc5f646d24
Jeffrey <lijw@berkeley.edu>
2016-05-14 23:24:21 -0400

    found out how to play mp3s simultaneously

---
+++ src/TestMP3Simultaneous.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Jeffrey Li on 5/14/2016.
+ */
+import java.io.*;
+
+import javazoom.jl.decoder.JavaLayerException;
+
+import javazoom.jl.player.*;
+
+public class TestMP3Simultaneous {
+    public static void main(String[] args) {
+        Thread t = new Thread(new MP3Runnable("C:\\Users\\Jeffrey Li\\drum-machine\\src\\clap.mp3"));
+        Thread t2 = new Thread(new MP3Runnable("C:\\Users\\Jeffrey Li\\drum-machine\\src\\open-hihat.mp3"));
+        t.start();
+        t2.start();
+    }
+}
