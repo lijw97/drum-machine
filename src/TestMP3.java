daafb283c49888747a9cd52424ebd5f838ef66ca
Jeffrey <lijw@berkeley.edu>
2016-05-14 21:02:03 -0400

    working mp3 test

---
+++ src/TestMP3.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Jeffrey Li on 5/14/2016.
+ */
+
+
+import java.io.*;
+
+import javazoom.jl.decoder.JavaLayerException;
+
+import javazoom.jl.player.*;
+
+public class TestMP3 {
+    public static void main(String[] args) {
+        try{
+            File file = new File("C:\\Users\\Jeffrey Li\\drum-machine\\src\\sample.mp3");
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            try {
+                Player play = new Player(bis);
+                play.play();
+
+            } catch (JavaLayerException jexcep) {
+
+            }
+        } catch (IOException ex) {
+
+        }
+    }
+}
