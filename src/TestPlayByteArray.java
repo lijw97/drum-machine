185e5262d9957afdca613cd18bd1bb38d313ae46
Jeffrey <lijw@berkeley.edu>
2016-06-02 15:41:59 -0400

    added tests

--- src/TestPlayByteArray.java
+++ src/TestPlayByteArray.java
@@ -27,73 +27,27 @@ public class TestPlayByteArray {
         long closeAfterStopDelay = 1;
         AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100.0f, 16, 1, 2, 44100.0f, false);
         try {
+            Clip clip = AudioSystem.getClip(); //generates a generic audio clip check API doc for more info
+            clip.open(format, finalbyte, 0, finalbyte.length);
+            clip.start();
+            clip.drain();
+            Thread.sleep(clip.getMicrosecondLength());
 
 
-            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-            SourceDataLine soundLine = (SourceDataLine) AudioSystem.getLine(info);
-            soundLine.open(format);
-            soundLine.start();
-            int nBytesRead = 0;
-            byte[] sampledData = new byte[finalbyte.length];
-            while (nBytesRead != -1) {
-                nBytesRead = audioInputStream.read(sampledData, 0, sampledData.length);
-                if (nBytesRead >= 0) {
-                    // Writes audio data to the mixer via this source data line.
-                    soundLine.write(sampledData, 0, nBytesRead);
-                }
-            }
-        } catch (UnsupportedAudioFileException ex) {
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } catch (LineUnavailableException ex) {
-            ex.printStackTrace();
-        } finally {
-            soundLine.drain();
-            soundLine.close();
+            Clip clip2 = AudioSystem.getClip(); //generates a generic audio clip check API doc for more info
+            clip2.open(format, finalbyte, 0, finalbyte.length);
+            clip2.start();
+
+            clip2.drain(); //DOES NOT BLOCK See Bug #4732218
+            Thread.sleep(clip2.getMicrosecondLength());
+            clip.stop();
+
+        } catch (LineUnavailableException e) {
+            System.out.println("hello");
         }
-//            Clip clip = AudioSystem.getClip(); //generates a generic audio clip check API doc for more info
-//            clip.open(format, finalbyte, 0, finalbyte.length);
-//            clip.addLineListener(new LineListener() {
-//                public void update(LineEvent le) {
-//                    //
-//                    // See BUG 4434125 for the reason of this rather interesting
-//                    // delay.
-//                    //
-//                    if (le.getType().equals(LineEvent.Type.STOP)) {
-//                        if (soundStopper != null) {
-//                            soundStopper.schedule(new TimerTask() {
-//                                public void run() {
-//                                    clip.close();
-//                                }
-//                            }, closeAfterStopDelay);
-//                        } else {
-//                            clip.close();
-//                        }
-//                    }
-//                }
-//            });
-            //
+        catch (InterruptedException e) {
 
-//            clip.start();
-
-
-//            Thread.sleep(clip.getMicrosecondLength());
-//
-//            Clip clip2 = AudioSystem.getClip(); //generates a generic audio clip check API doc for more info
-//            clip2.open(format, finalbyte, 0, finalbyte.length);
-//            clip2.start();
-//
-//            clip2.drain(); //DOES NOT BLOCK See Bug #4732218
-
-//            clip.stop();
-
-//        } catch (LineUnavailableException e) {
-//            System.out.println("hello");
-//        }
-//        catch (InterruptedException e) {
-//
-//        }
+        }
 
 
     }
